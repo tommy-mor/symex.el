082886710c8e124fd65e0f4c74c94d6122360b27
Tommy <thmorriss@gmail.com>
2021-09-16 20:38:51 -0400

    Update symex-evil.el

--- symex-evil.el
+++ symex-evil.el
@@ -3,7 +3,7 @@
 ;; URL: https://github.com/countvajhula/symex.el
 
 ;; This program is "part of the world," in the sense described at
-;; https://drym.org.  From your perspective, this is no different than
+;; http://drym.org.  From your perspective, this is no different than
 ;; MIT or BSD or other such "liberal" licenses that you may be
 ;; familiar with, that is to say, you are free to do whatever you like
 ;; with this program.  It is much more than BSD or MIT, however, in
@@ -20,8 +20,9 @@
 ;;
 
 ;;; Commentary:
-
+;;
 ;; Evil modal frontend to symex.
+;;
 
 ;;; Code:
 
@@ -39,7 +40,7 @@
 
 (define-minor-mode symex-editing-mode
   "Minor mode to modulate keybindings in symex evil state."
-  :lighter " symex"
+  :lighter "symex"
   :keymap symex-editing-mode-map)
 
 (evil-define-state symex
@@ -64,111 +65,101 @@ executing this command to get the expected behavior."
 ;; TODO: others that could accept a count argument:
 ;; simple insert/append
 (defvar symex--evil-keyspec
-  '(("h" . symex-go-backward)
-    ("j" . symex-go-down)
-    ("k" . symex-go-up)
-    ("l" . symex-go-forward)
-    ("gj" . symex-next-visual-line)
-    ("gk" . symex-previous-visual-line)
-    ("(" . symex-create-round)
-    ("[" . symex-create-square)
+  '(("h" . symex-go-down)
+    ("n" . symex-go-forward)
+    ("e" . symex-go-backward)
+    ("i" . symex-go-up)
+	("(" . symex-create-round)
+	("[" . symex-create-square)
+    ("{" . symex-create-curly)
     (")" . symex-wrap-round)
     ("]" . symex-wrap-square)
-    ("C-'" . symex-cycle-quote)
-    ("C-," . symex-cycle-unquote)
-    ("`" . symex-add-quoting-level)
-    ("C-`" . symex-remove-quoting-level)
+    ("}" . symex-wrap-curly)
     ("f" . symex-traverse-forward)
+    ("w" . symex-traverse-forward)
     ("b" . symex-traverse-backward)
     ("C-f" . symex-traverse-forward-more)
     ("C-b" . symex-traverse-backward-more)
     ("F" . symex-traverse-forward-skip)
     ("B" . symex-traverse-backward-skip)
-    ("{" . symex-leap-backward)
-    ("}" . symex-leap-forward)
-    ("M-{" . symex-soar-backward)
-    ("M-}" . symex-soar-forward)
-    ("C-k" . symex-climb-branch)
-    ("C-j" . symex-descend-branch)
-    ("y" . symex-yank)
-    ("Y" . symex-yank-remaining)
+    ("C-n" . symex-soar-forward)
+    ("C-e" . symex-soar-backward)
+    ("C-S-n" . symex-leap-forward) ;; WHY DOES THIS NOT WORK????????
+    ("C-S-e" . symex-leap-backward)
+    ("C-i" . symex-climb-branch)
+    ("C-h" . symex-descend-branch)
     ("p" . symex-paste-after)
+    ("y" . symex-yank)
     ("P" . symex-paste-before)
     ("x" . symex-delete)
-    ("X" . symex-delete-backwards)
-    ("D" . symex-delete-remaining)
     ("c" . symex-change)
-    ("C" . symex-change-remaining)
-    ("C--" . symex-clear)
+    ("C" . symex-clear)
     ("s" . symex-replace)
     ("S" . symex-change-delimiter)
-    ("H" . symex-shift-backward)
-    ("L" . symex-shift-forward)
-    ("M-H" . symex-shift-backward-most)
-    ("M-L" . symex-shift-forward-most)
-    ("K" . paredit-raise-sexp) ; revisit kb
-    ("C-S-j" . symex-emit-backward)
+    ("E" . symex-shift-backward)
+    ("N" . symex-shift-forward)
+    ("H" . paredit-raise-sexp)		   ; revisit kb
+    ;("C-S-e" . symex-emit-backward)	   ; TODO convert these to colemak
     ("C-(" . symex-capture-backward)
-    ("C-S-h" . symex-capture-backward)
-    ("C-{" . symex-emit-backward)
-    ("C-S-l" . symex-capture-forward)
-    ("C-}" . symex-emit-forward)
-    ("C-S-k" . symex-emit-forward)
+    ;; ("C-S-e" . symex-capture-backward)
+    ;; ("C-{" . symex-emit-backward)
+    ;; ("C-S-n" . symex-capture-forward)
+    ;; ("C-}" . symex-emit-forward)
+    ;; ("C-S-e" . symex-emit-forward)
     ("C-)" . symex-capture-forward)
     ("z" . symex-swallow)
     ("Z" . symex-swallow-tail)
-    ("e" . symex-evaluate)
-    ("E" . symex-evaluate-remaining)
-    ("C-M-e" . symex-evaluate-pretty)
+    ("R" . symex-evaluate)
+    ("M-E" . symex-evaluate-pretty)
     ("d" . symex-evaluate-definition)
-    ("M-e" . symex-eval-recursive)
-    ("T" . symex-evaluate-thunk)
-    (":" . eval-expression)
+    ("C-M-e" . symex-eval-recursive)
+    ("T" . symex-evaluate-thunk) ; switch these to be next/prev buffer
+	(":" . evil-ex)
     ("t" . symex-switch-to-scratch-buffer)
     ("M" . symex-switch-to-messages-buffer)
     ("r" . symex-repl)
-    ("R" . symex-run)
-    ("|" . symex-split)
-    ("&" . symex-join)
-    ("-" . symex-splice)
+										;("R" . symex-run) ; not sure what these do
+    ("X" . symex-run)
+    ("|" . lispy-split)
+    ("m" . symex-join)
+    ("\\" . symex-splice)
     ("o" . symex-open-line-after)
     ("O" . symex-open-line-before)
-    (">" . symex-insert-newline)
-    ("<" . symex-join-lines-backwards)
-    ("C->" . symex-append-newline)
-    ("C-<" . symex-join-lines)
+	(">" . symex-capture-forward)
+    ("<" . symex-capture-backward)
+    ("C->" . symex-emit-forward)
+    ("C-<" . symex-emit-backward)
+
+										;(">" . symex-insert-newline)
+										;("<" . symex-join-lines-backwards)
+										;("C->" . symex-append-newline)
+										;("C-<" . symex-join-lines)
     ("C-S-o" . symex-append-newline)
-    ("J" . symex-join-lines)
-    ("M-J" . symex-collapse)
-    ("M-<" . symex-collapse)
-    ("M->" . symex-unfurl)
-    ("C-M-<" . symex-collapse-remaining)
-    ("C-M->" . symex-unfurl-remaining)
+    ("N" . symex-join-lines)
+    ("M-N" . symex-collapse)
     ("0" . symex-goto-first)
-    ("M-h" . symex-goto-first)
+    ("M-n" . symex-goto-first)
     ("$" . symex-goto-last)
-    ("M-l" . symex-goto-last)
-    ("M-j" . symex-goto-lowest)
-    ("M-k" . symex-goto-highest)
+    ("M-e" . symex-goto-last)
+    ("M-h" . symex-goto-lowest)
+    ("M-i" . symex-goto-highest)
     ("=" . symex-tidy)
     ("<tab>" . symex-tidy)
-    ("C-=" . symex-tidy-remaining)
-    ("C-<tab>" . symex-tidy-remaining)
     ("M-=" . symex-tidy-proper)
-    ("M-<tab>" . symex-tidy-proper)
+    ("M-<tab>" . symex-tidy)
     ("A" . symex-append-after)
     ("a" . symex-insert-at-end)
-    ("i" . symex-insert-at-beginning)
-    ("I" . symex-insert-before)
-    ("w" . symex-wrap)
+	("u" . symex-insert-at-beginning)
+    ("U" . symex-insert-before)
+    ("W" . symex-wrap)
     ("C-d" . symex--evil-scroll-down)
     (";" . symex-comment)
-    ("M-;" . symex-comment-remaining)
-    ("C-;" . symex-eval-print) ; weird pre-offset (in both)
+    ("C-;" . symex-eval-print)			; weird pre-offset (in both)
     ("s-;" . symex-evaluate)
     ("H-h" . symex--toggle-highlight) ; treats visual as distinct mode
     ("?" . symex-describe)
     ("<return>" . symex-enter-lower)
+    ("C-<escape>" . symex-enter-lower)
     ("<escape>" . symex-escape-higher))
   "Key specification for symex evil state.")
 
